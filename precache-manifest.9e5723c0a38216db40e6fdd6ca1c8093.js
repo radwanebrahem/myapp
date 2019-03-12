2275bafd89b8554b805b85e1a819c74577c10e73
radwanebrahem <eng.radwan.ebrahem@gmail.com>
2019-03-12 23:38:32 +0200

    Updates

---
+++ precache-manifest.9e5723c0a38216db40e6fdd6ca1c8093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b64c3fc3a9dd96a97b5c",
+    "url": "/static/js/main.9468bd2c.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "b64c3fc3a9dd96a97b5c",
+    "url": "/static/css/main.09f1c528.chunk.css"
+  },
+  {
+    "revision": "c6571df68d5147571e35f70edf9f55b8",
+    "url": "/index.html"
+  }
+];
